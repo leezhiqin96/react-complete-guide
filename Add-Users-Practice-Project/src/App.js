68d0144742bc397a547c934e2515e21f89bf07d1
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-05-24 16:31:14 +0800

    Edited card import location

--- Add-Users-Practice-Project/src/App.js
+++ Add-Users-Practice-Project/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-import Card from "./components/UI/Card";
 import AddUser from "./components/Users/AddUser";
 
 function App() {
   return (
-    <Card>
+    <div>
       <AddUser />
-    </Card>
+    </div>
   );
 }
 
