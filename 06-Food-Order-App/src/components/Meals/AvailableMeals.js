c6770c6d0861c48d87dcf6104f4c4df019c981e4
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-06-02 11:59:49 +0800

    Adding 'MealItem' Component

--- 06-Food-Order-App/src/components/Meals/AvailableMeals.js
+++ 06-Food-Order-App/src/components/Meals/AvailableMeals.js
@@ -1,3 +1,6 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -28,11 +31,20 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      description={meal.description}
+      price={meal.price}
+      name={meal.name}
+    />
+  ));
 
   return (
     <section className={classes.meal}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
