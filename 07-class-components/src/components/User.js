b8531b5ecccd6a18e7f453dfe2c4b9c4fa13c96f
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-06-16 15:25:15 +0800

    Replacing functional component with class-based component

--- 07-class-components/src/components/User.js
+++ 07-class-components/src/components/User.js
@@ -1,7 +1,17 @@
-import classes from './User.module.css';
+import React, { Component } from "react";
+import classes from "./User.module.css";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// class-based
+// Component class adds important functionality to make your class work as a component
+class User extends Component {
+  render() {
+    // by extending Component class, can now use this.props
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
