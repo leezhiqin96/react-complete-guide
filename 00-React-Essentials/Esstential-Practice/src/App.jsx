c7824fcda1e5c4fea3bc810ce25123697e018d8e
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-12-06 18:49:34 +0800

    React Essential practice

--- 00-React-Essentials/Esstential-Practice/src/App.jsx
+++ 00-React-Essentials/Esstential-Practice/src/App.jsx
@@ -1,6 +1,35 @@
+import { useState, Fragment } from "react";
+
+import CalculatorForm from "./components/CalculatorForm.jsx";
+import Header from "./components/Header.jsx";
+import Results from "./components/Results.jsx";
+
 function App() {
+  const [ userInput, setUserInput ] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue         //Change the newValue into an int
+      }
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
+    <Fragment>
+      <Header />
+      <CalculatorForm userInput={userInput} onChangeInput={handleChange} />
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Results input={userInput} />}
+    </Fragment>
   )
 }
 
